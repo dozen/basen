cd66d74f26571ac6023239fa37001d6e2b87239b
DOZEN <kyosuke6428@gmail.com>
2012-02-25 03:27:51 +0900

    first commit

---
+++ basen.php
@@ -0,0 +1,34 @@
+<?php
+class Basen {
+  function __construct() {
+    $charfile = file('char');
+    $this->char = $charfile[0];
+    unset($charfile);
+    $charset = mb_detect_encoding($this->char);
+    mb_internal_encoding($charset);
+  }
+  function encode($value, $base) {
+    $result = null;
+    $place = floor(log($value, $base)); //桁数を求める(実際の桁数-1)
+    while ($place >= 0) {
+      $pow = pow($base, $place);
+      $currentplace = floor($value / $pow); //現在の桁の数
+      $result .= mb_substr($this->char, $currentplace, 1); //文字に変換
+      $value -= $currentplace * $pow; //現在の桁の分だけ引き算
+      $place--;
+    }
+    return $result;
+  }
+  function decode($value, $base) {
+    $result = null;
+    $currentplace = null;
+    $place = mb_strlen($value) - 1; //桁数を求める
+    while ($place >= $currentplace) {
+      $pow = pow($base, $place - $currentplace);
+      $currentplacechar = mb_substr($value, $currentplace, 1); //現在の桁の文字を抽出
+      $result += mb_strpos($this->char, $currentplacechar) * $pow;
+      $currentplace++;
+    }
+    return $result;
+  }
+}
